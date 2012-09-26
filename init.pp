f8af70c89173d43ee39191d26f282bee0c612706
Michal Huniewicz <michal.huniewicz.registered@gmail.com>
2012-09-26 20:33:13 +0100

    Added Development folder directories; mysql-server and emacs packages.

--- init.pp
+++ init.pp
@@ -1,3 +1,35 @@
+$username = 'm1key'
+
+user { "$username":
+  ensure => present,
+}
+
+$development_dir = "/home/$username/Development"
+file { "$development_dir":
+  ensure => directory
+}
+file { "$development_dir/Downloaded Software":
+  ensure => directory
+}
+file { "$development_dir/Servers":
+  ensure => directory
+}
+file { "$development_dir/Tools":
+  ensure => directory
+}
+file { "$development_dir/Projects":
+  ensure => directory
+}
+file { "$development_dir/Temp":
+  ensure => directory
+}
+
 package {'maven':
+  ensure => latest,
+}
+package {'mysql-server':
+  ensure => latest,
+}
+package {'emacs':
   ensure => latest,
 }
